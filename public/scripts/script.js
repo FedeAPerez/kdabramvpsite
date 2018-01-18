8f494e235de08217e82d40d3cf98080334a026db
Federico Perez <fedeaperez@hotmail.com.ar>
2018-01-17 23:33:09 -0300

    kquer

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,8 @@
+
 $(document).ready(function(){
-	alert("llego a js");
 	$("#idBtnStart").click(function (){
 		$('html, body').animate({
-			alert("estoy ejecutando");
-        	scrollTop: $("#content").offset().top;
+        	scrollTop: $("#content").offset().top
     	}, 1500);
 	});
 });
